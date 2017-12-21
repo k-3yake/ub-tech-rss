6c839ceed3c36c84b3f66d5a0110b90398c12997
Katsuki Miyake <3katsu3hiro@gmail.com>
2017-12-22 03:09:21 +0900

    仮実装

--- ub-tech-rss-app/src/main/java/org/k3yake/rss/PrintRSSApplication.java
+++ ub-tech-rss-app/src/main/java/org/k3yake/rss/PrintRSSApplication.java
@@ -1,11 +1,31 @@
 package org.k3yake.rss;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 /**
  * Created by katsuki on 17/12/22.
  */
 public class PrintRSSApplication {
 
     public static void main(String[] args){
-        System.out.println("ほげ");
+        String url = "http://localhost:8080/rss/";
+
+        try{
+            CloseableHttpClient client = HttpClients.createDefault();
+            HttpGet get = new HttpGet( url );
+            CloseableHttpResponse response = client.execute( get );
+            int sc = response.getStatusLine().getStatusCode(); //. 200 の想定
+            HttpEntity entity = response.getEntity();
+            String html = EntityUtils.toString( entity, "UTF-8" );
+            System.out.println( html.replaceAll("NewsPicks","") ); //. 取得結果をコンソールへ
+            client.close();
+        }catch( Exception e ){
+            e.printStackTrace();
+        }
     }
 }
